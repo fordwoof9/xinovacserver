7a2abd952db5505350542414b53f55c3b3d3a14b
fordwoof9 <fordwoof9@outlook.com>
2021-11-19 23:16:22 +0700

    10

--- app.js
+++ app.js
@@ -41,8 +41,6 @@ app.get("/", (req, res) => {
 const bookingRoom = require("./routes/bookingRoute")
 app.use('/api/booking', bookingRoom)
 
-
-
 console.log('starting server...')
 const port = process.env.PORT || 5000;
 app.listen(port, () => { console.log(`server is running on port ${port}.`)})
