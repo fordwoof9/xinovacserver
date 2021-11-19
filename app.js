f985601553ac21dde5ff8f8f0c4c07bee82f853d
fordwoof9 <fordwoof9@outlook.com>
2021-11-19 21:59:14 +0700

    9

--- app.js
+++ app.js
@@ -6,13 +6,36 @@ const helmet = require('helmet')
 
 const app = express()
 
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+  }
+
+const domainsFromEnv = process.env.CORS_DOMAINS || ""
+const whitelist = domainsFromEnv.split(",").map(item => item.trim())
+
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true,
+  }
+
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(cors({credentials: true, origin: ['http://localhost:3000/', 'https://xinovac.herokuapp.com/', '*']}))
+//app.use(cors({credentials: true, origin: ['http://localhost:3000/', 'https://xinovac.herokuapp.com/', '*']}))
+app.use(cors(corsOptions))
 
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: false }))
 
 app.use(helmet())
+
+app.get("/", (req, res) => {
+    res.send({ message: "Welcome to xinovac API!" })
+  })
 
 //ROUTES
 const bookingRoom = require("./routes/bookingRoute")
